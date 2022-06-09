5839b6d174a84f34d74851a01f693909582d5b30
manuelibarbia <manuelibarbia02@gmail.com>
2022-06-09 20:38:58 -0300

    Alternativa ejercicio 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,8 +1,22 @@
-lista_1 = [1, 2 ,3, 4]
-lista_2 = [3, 4, 5]
+# lista_1 = [1, 2 ,3, 4]
+# lista_2 = [3, 4, 5]
 
-intersection = set(lista_1) & set(lista_2)
-if len(intersection) >= 2:
-    print ("Parecidas")
+# intersection = set(lista_1) & set(lista_2)
+# if len(intersection) >= 2:
+#     print ("Parecidas")
+# else:
+#     print("No coinciden")
+
+list_1 = [1, 2, 3, 4, 5]
+list_2 = [2, 3, 4]
+
+counter = 0
+for x in range(len(list_1)):
+    for i in range(len(list_2)):
+        if list_1[x] == list_2[i]:
+            counter +=1
+
+if counter >= 2:
+    print("Parecidas")
 else:
     print("No coinciden")
